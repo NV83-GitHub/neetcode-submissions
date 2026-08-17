9f4b779b573b3d79c17bec8b28a0c1cef3090611
Nicolas Vandenbruaene <nicolas.vandenbruaene@gmail.com>
2026-08-17 14:27:16 +0200

    Add: python-variable-types - submission-0

---
+++ Python For Beginners/python-variable-types/submission-0.py
@@ -0,0 +1,12 @@
+integer_type = 42
+float_type = 42.5
+bool_type = True
+string_type = "Python is cool since 1991"
+list_type = ["Bob","Patrick","Handsome Squidward"]
+
+
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
