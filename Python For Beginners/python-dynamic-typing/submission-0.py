a1f1c00c68b75487d95b79a563484c74c9ab0475
Nicolas Vandenbruaene <nicolas.vandenbruaene@gmail.com>
2026-08-17 14:40:41 +0200

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,19 @@
+variable = 10
+
+print(type(variable))
+
+variable = 10.5
+
+print(type(variable))
+
+variable = False
+
+print(type(variable))
+
+variable = "false"
+
+print(type(variable))
+
+variable = ["f","a","l","s","e"]
+
+print(type(variable))
